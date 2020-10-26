88da9cd5fb166ecc3c7185e89c4822952135c580
dominic_flocea <dominic.flocea@nitroagility.com>
2020-10-26 22:10:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (rec, res) => {
+    res.json({
+        'message': 'full stack message board! ☁'
+    });
+}); 
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`)
+});
