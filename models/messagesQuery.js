a6b56714893bf84ea5cb1deda2c89cfaac1a0e79
dominatorfaf <dominicflocea.dev@gmail.com>
2020-10-29 14:32:47 +0200

    Added message schema and create message operation

--- models/messagesQuery.js
+++ models/messagesQuery.js
@@ -1,9 +1,26 @@
 const db = require('../db/connection');
 
+const schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    subject: Joi.string().required(),
+    message: Joi.string().max(500).required(),
+    imageURL: Joi.string().uri({
+        scheme: [
+            /https?/
+        ]
+    })
+});
+
 const messages = db.get('messsages');
 
 function getAll() {    
     return messages.find();
+}
+
+function create(message) {
+    const result = Joi.validate(message);
+
+    return messages.insert(message);
 }
 
 module.exports = {
